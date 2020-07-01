eb826c273e6ae190f11e6f902ee43221a62a33c7
HristovCodes <ivailohristov1990@gmail.com>
2020-07-01 15:38:00 +0300

    Updates

---
+++ precache-manifest.b5fccafc14edbcb03130928c386a017d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdb6937aee6e9dbfb411b2f104f03f0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "710e8abc62372c42d4f2",
+    "url": "/portfolio/static/css/main.4b7e2590.chunk.css"
+  },
+  {
+    "revision": "e4abadc880c3d4506637",
+    "url": "/portfolio/static/js/2.516d54bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.516d54bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710e8abc62372c42d4f2",
+    "url": "/portfolio/static/js/main.8afb5b18.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
